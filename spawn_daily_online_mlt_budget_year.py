27b70e1a42eb2f482c2c7b7adebed10851a40dd7
Ryan Holmes <holmes.ryan.m@gmail.com>
2025-08-16 20:27:20 +1000

    Add working standard average budget climatology code and plotting, including SW/latent/sensible/LW.

---
+++ spawn_daily_online_mlt_budget_year.py
@@ -0,0 +1,30 @@
+# /usr/bin/env python3
+
+import numpy as np
+import os
+import fileinput
+import time as time_sleeper
+
+outputs = np.arange(337,338)
+sleep_time = 0
+dry = False
+
+# Submit a job for each year:
+os.chdir('tmp_qsub_scripts/')
+for output in outputs:
+    arguments = {'XXOUTPUTXX':str(output),
+                    }
+
+    fscr = 'daily_online_mlt_budget_year_' + str(output) + '.sub'
+    os.system('cp ../daily_online_mlt_budget_year.sub ' + fscr)
+    with fileinput.FileInput(fscr, inplace=True) as file:
+        for line in file:
+            line_out = line
+            for arg in arguments.keys():
+                line_out = line_out.replace(arg,arguments[arg])
+            print(line_out, end='')
+    if not dry:
+        os.system('qsub ' + fscr)
+    if not dry:
+        print('Waiting %02ds after output %04d' % (sleep_time,output))
+        time_sleeper.sleep(sleep_time)
